156bbcbf4d0ab7c33e394fb67bc586b8b29b92ea
Alberto L. Figueroa <0234621@up.edu.mx>
2023-11-17 00:31:46 -0600

    Refactor code for improved readability and performance.

--- src/routes/meds.routes.js
+++ src/routes/meds.routes.js
@@ -17,11 +17,51 @@ const prisma = new PrismaClient();
 //     receta            receta[]
 //   }
 
+// enum medicinas_Criticidad {
+//     Alto
+//     Medio
+//     Bajo
+//   }
+
 router_meds.get('/totalMeds', async (req, res) => {
     const meds = await prisma.medicinas.count();
     res.json(meds);
 }
 )
+
+const criticidadMap = {
+    'Alto': 3,
+    'Medio': 2,
+    'Bajo': 1
+};
+
+// Get the medicines with the most critical level and lowest inventory level
+router_meds.get('/critical', async (req, res) => {
+    // Fetch all medicines
+    const meds = await prisma.medicinas.findMany();
+
+    // Sort medicines
+    meds.sort((a, b) => {
+        // Compare Criticidad
+        const criticidadA = criticidadMap[a.Criticidad];
+        const criticidadB = criticidadMap[b.Criticidad];
+        if (criticidadA > criticidadB) {
+            return -1;
+        }
+        if (criticidadA < criticidadB) {
+            return 1;
+        }
+
+        // If Criticidad is equal, compare NivelDeInventario
+        return a.NivelDeInventario - b.NivelDeInventario;
+    });
+
+    // Return the first 3 medicines
+    res.json(meds.slice(0, 5));
+});
+
+
+
 
 
 
