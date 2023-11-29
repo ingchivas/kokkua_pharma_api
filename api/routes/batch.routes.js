7dcc8b59d950890d0e923456be59ecf061b94514
Alberto L. Figueroa <0234621@up.edu.mx>
2023-11-28 22:55:15 -0600

    Add batch routes and endpoints

--- api/routes/batch.routes.js
+++ api/routes/batch.routes.js
@@ -15,11 +15,49 @@ const prisma = new PrismaClient();
 //     UbicacionAlmacen String?      @db.VarChar(100)
 //     proveedores      proveedores? @relation(fields: [IDProveedor], references: [IDProveedor], onDelete: NoAction, onUpdate: NoAction, map: "lote_ibfk_1")
 //     medicinas        medicinas?   @relation(fields: [IDMedicina], references: [IDMedicina], onDelete: NoAction, onUpdate: NoAction, map: "lote_ibfk_2")
-  
+
 //     @@index([IDMedicina], map: "IDMedicina")
 //     @@index([IDProveedor], map: "IDProveedor")
 //   }
-  
+
+
+// Get all batches, include medicine name, description, and provider name
+router_batch.get('/getBatches', async (req, res) => {
+    const batches = await prisma.lote.findMany({
+        include: {
+            medicinas: {
+                select: {
+                    NombreMedicina: true,
+                    Descripci_n: true
+                }
+            },
+            proveedores: {
+                select: {
+                    Nombre: true
+                }
+            }
+        }
+    });
+
+    const processedBatches = batches.map(batch => {
+        return {
+            ...batch,
+            // Remove medicinas and proveedores from batch
+            medicinas: undefined,
+            proveedores: undefined,
+
+            NombreMedicina: batch.medicinas.NombreMedicina,
+            Descripci_n: batch.medicinas.Descripci_n,
+            NombreProveedor: batch.proveedores.Nombre,
+            Expirado : batch.FechaExpiraci_n < new Date() ? true : false
+        }
+    })
+
+
+    res.json(processedBatches);
+}
+)
+
 
 
 // Obtain percentage of expired meds in all batches
@@ -31,7 +69,7 @@ router_batch.get('/getExpiredPercentage', async (req, res) => {
             }
         }
     });
-    
+
     const expiredPercentagePrevMonth = await prisma.lote.count({
         where: {
             FechaExpiraci_n: {
@@ -44,18 +82,42 @@ router_batch.get('/getExpiredPercentage', async (req, res) => {
     let percentage = (expiredPercentage / total * 100).toString()
 
     let expiredPercentagePrevMonthPercentage = (expiredPercentagePrevMonth / total * 100).toString()
-    expiredPercentagePrevMonthPercentage = expiredPercentagePrevMonthPercentage.slice(0, (expiredPercentagePrevMonthPercentage.indexOf("."))+3);
+    expiredPercentagePrevMonthPercentage = expiredPercentagePrevMonthPercentage.slice(0, (expiredPercentagePrevMonthPercentage.indexOf(".")) + 3);
 
 
-    percentage = percentage.slice(0, (percentage.indexOf("."))+3);
+    percentage = percentage.slice(0, (percentage.indexOf(".")) + 3);
     res.json({
         "expiredPercentage": parseFloat(percentage),
         "expiredPercentagePrevMonth": parseFloat(expiredPercentagePrevMonthPercentage),
         "total": total,
-        "delta" : parseFloat((parseFloat(percentage) - parseFloat(expiredPercentagePrevMonthPercentage)).toString().slice(0, ((parseFloat(percentage) - parseFloat(expiredPercentagePrevMonthPercentage)).toString().indexOf("."))+3))
+        "delta": parseFloat((parseFloat(percentage) - parseFloat(expiredPercentagePrevMonthPercentage)).toString().slice(0, ((parseFloat(percentage) - parseFloat(expiredPercentagePrevMonthPercentage)).toString().indexOf(".")) + 3))
     });
-    }
+}
 )
+
+// Register a new batch
+router_batch.post('/registerBatch', async (req, res) => {
+    const { IDMedicina, FechaManufactura, FechaExpiraci_n, IDProveedor, CantidadRecibida, UbicacionAlmacen } = req.body;
+
+    try {
+        const newBatch = await prisma.lote.create({
+            data: {
+                IDMedicina,
+                FechaManufactura,
+                FechaExpiraci_n,
+                IDProveedor,
+                CantidadRecibida,
+                CantidadRestante: CantidadRecibida,
+                UbicacionAlmacen
+            }
+        });
+        res.json(newBatch);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+})
 
 
 
